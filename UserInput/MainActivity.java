bb64827c939680deb919f6850b40795ee008c46a
Jonathan Scialpi <scialpi27@gmail.com>
2017-03-14 14:34:17 -0400

    Create MainActivity.java

---
+++ UserInput/MainActivity.java
@@ -0,0 +1,46 @@
+package com.android.roguebot.justjava;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import java.text.NumberFormat;
+
+public class MainActivity extends AppCompatActivity {
+int quantity =2;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    /**
+     * This method is called when the order button is clicked.
+     */
+    public void submitOrder(View view) {
+        display(quantity);
+        displayPrice(quantity * 5);
+    }
+
+    public void increment(View view) {
+        display(++quantity);
+    }
+
+    public void decrement(View view) {
+        display(--quantity);
+    }
+
+    /**
+     * This method displays the given quantity value on the screen.
+     */
+    private void display(int number) {
+        TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
+        quantityTextView.setText("" + number);
+    }
+
+    private void displayPrice(int number) {
+        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
+    }
+
+}
